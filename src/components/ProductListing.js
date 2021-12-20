67648d2731f5e6e77c763982968e164e8a6891ce
Warfan-Ahmad <warfanahmad@gmail.com>
2021-12-20 22:55:03 +0530

    Ready to Deploy

---
+++ src/components/ProductListing.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import React,{useEffect} from 'react'
+import ProductComponent from './ProductComponent'
+import {setProducts} from '../Redux/actions/productAction'
+import { useDispatch, useSelector } from 'react-redux'
+import logo from './YinandYang.gif'
+
+function ProductListing() {
+    const dispatch = useDispatch()
+    
+    const fetchProducts = async () =>{
+        const response =  await axios.get("https://fakestoreapi.com/products").catch((error)=>{
+            alert(error)
+        })
+        dispatch(setProducts(response.data))
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+
+    return (
+        <div className='ui grid container'>
+            <ProductComponent />
+        </div>
+    )
+}
+
+export default ProductListing
+
